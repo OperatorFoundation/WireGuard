3cd75f3c083e44b5fc6257d527079b094efa0139
Brandon Wiley <brandon@blanu.net>
2017-11-20 16:07:51 -0700

    Initial Commit

---
+++ WireGuard/WireGuard.h
@@ -0,0 +1,19 @@
+//
+//  WireGuard.h
+//  WireGuard
+//
+//  Created by Brandon Wiley on 11/20/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WireGuard.
+FOUNDATION_EXPORT double WireGuardVersionNumber;
+
+//! Project version string for WireGuard.
+FOUNDATION_EXPORT const unsigned char WireGuardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WireGuard/PublicHeader.h>
+
+

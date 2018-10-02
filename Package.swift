4f5086ef955ebb20e35bf4b91d7a00ecbe0d4219
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-10-02 14:57:00 -0500

    Got compilation to work by manually adding libsodium-ios.a

--- Package.swift
+++ Package.swift
@@ -14,13 +14,17 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/swift-sodium.git", from: "0.5.3"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.7.2"),
+        .package(url: "https://github.com/OperatorFoundation/HKDFKit.git", from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/Blake2.git", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "WireGuard",
-            dependencies: []),
+            dependencies: ["Sodium", "CryptoSwift", "HKDFKit", "Blake2"]),
         .testTarget(
             name: "WireGuardTests",
             dependencies: ["WireGuard"]),

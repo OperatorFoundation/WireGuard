34fc5bbf519fe8fd7d4d5976ad9d997c58953be1
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-10-02 12:14:50 -0500

    Transitioned from CocoaPods to Swift Package Manager

--- Pods/Target Support Files/Pods-WireGuardTests/Pods-WireGuardTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WireGuardTests : NSObject
-@end
-@implementation PodsDummy_Pods_WireGuardTests
-@end
--- Tests/WireGuardTests/WireGuardTests.swift
+++ Tests/WireGuardTests/WireGuardTests.swift
@@ -1,16 +1,42 @@
+//
+//  WireGuardTests.swift
+//  WireGuardTests
+//
+//  Created by Brandon Wiley on 11/20/17.
+//  Copyright © 2017 Operator Foundation. All rights reserved.
+//
+
 import XCTest
+
 @testable import WireGuard
 
-final class WireGuardTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(WireGuard().text, "Hello, World!")
+class WireGuardTests: XCTestCase {
+    var spubr: Data?
+    var sprivr: Data?
+    var spubi: Data?
+    var sprivi: Data?
+
+    override func setUp() {
+        super.setUp()
+        
+        var pub: Data
+        var priv: Data
+        
+        (pub, priv) = DHgenerate()
+        spubr=pub
+        sprivr=priv
+        
+        (pub, priv) = DHgenerate()
+        spubi=pub
+        sprivi=priv
     }
-
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testInitiation() {
+        let initiation=HandshakeInitiation(spubr: spubr!, sprivi: sprivi!, maybeStaticKey: nil)
+        let initData=initiation.encode()
+    }
 }
